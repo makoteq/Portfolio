f7bd729c09857b51d370c107019d1dbc2736df5b
Makotek <makoteq@gmail.com>
2019-03-14 19:52:35 +0100

    transition :dog:

--- src/components/moon.vue
+++ src/components/moon.vue
@@ -1,39 +1,47 @@
 <template>
-    <font-awesome-icon :style="{color:theme}" @click="changeTheme" class="iconMoon" :icon="{ prefix: 'fas', iconName: 'cloud-sun' }"/>
+    <font-awesome-icon
+      :style="{color:theme}"
+      @click="changeTheme"
+      class="iconMoon"
+      :icon="{ prefix: 'fas', iconName: 'cloud-sun' }"
+    />
 </template>
 
 <script>
 export default {
-    name:'moon',
-    data() {
-        return {
-            color:'white'
-        }
-    },
-    methods:{
-        changeTheme:function(){
-            if(this.color=='white'){
-                this.color='rgb(12, 179, 12)'
-            }else{
-                this.color='white'
-            }
-            this.$emit("changeTheme",this.color)
-        }
-    },
-      props:{
-    theme:String
+  name: "moon",
+  data() {
+    return {
+      color: this.theme,
+    };
+  },
+  methods: {
+    changeTheme: function() {
+        this.show=!this.show
+      if (this.color == "rgb(12, 179, 12)") {
+        this.color = "white";
+      } else {
+        this.color = "rgb(12, 179, 12)";
+      }
+      this.$emit("changeTheme", this.color);
+    }
+  },
+  props: {
+    theme: String
   }
-}
+};
 </script>
 
-<style>
-.iconMoon{ 
-    font-size:2em;
-    position:absolute;
-    right:3%;
-    top:3%;
-    cursor: pointer;
-    }
-
-    
+<style skoped>
+.iconMoon {
+  font-size: 2em;
+  position: absolute;
+  right: 3%;
+  top: 3%;
+  cursor: pointer;
+  transition:0.2s linear;
+}
+.iconMoon:hover {
+  transform: scale(1.5)
+}
 </style>
